0bbee3c444aeb08854f69c3ca24794c98dce5d39
pavelkuzkin <pavelkuzkin@gmail.com>
2022-12-05 21:26:04 -0800

    P3 part 2

--- 2022/3-rucksacks/solved.py
+++ 2022/3-rucksacks/solved.py
@@ -16,5 +16,17 @@ def solution_part1():
         total += item_priority(item_in_both)
     print('part 1 answer', total)
 
+def solution_part2():
+    f = open("./input.txt", "r")
+    total = 0
+    current_group = []
+    for x in f:
+        current_group.append(x.strip())
+        if (len(current_group) == 3):
+            badge = (set(current_group[0]) & set(current_group[1]) & set(current_group[2])).pop()
+            total += item_priority(badge)
+            current_group = []
+    print('part 2 answer', total)
 
 solution_part1()
+solution_part2()
