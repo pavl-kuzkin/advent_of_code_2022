a73bc9daae0d383ef351c7e8856c0124d4e53229
pavelkuzkin <pavelkuzkin@gmail.com>
2022-12-06 19:49:44 -0800

    P5 part 2

--- 2022/5-move-crate-stacks/solved.py
+++ 2022/5-move-crate-stacks/solved.py
@@ -55,5 +55,35 @@ def solution_part1():
     print('part 1 answer', ''.join(answer))
 
 
-solution_part1()
-# solution_part2()
+def solution_part2():
+    crates = []
+    for x in range(0, 10):
+        crates.append([])
+    f = open("./input.txt", "r")
+    for x in f:
+        if x.startswith(" 1   2   3"):
+            # all stacks have been read in reverse order so reverse will get us the real order
+            for stack in crates:
+                stack.reverse()
+        elif x.startswith("move"):
+            moves = [int(s) for s in x.split() if s.isdigit()]
+            # print(moves)
+            item_count = moves[0]
+            from_idx = moves[1]
+            to_idx = moves[2]
+            from_stack = crates[from_idx]
+            to_stack = crates[to_idx]
+            to_stack.extend(from_stack[-1*item_count:])
+            del from_stack[-1*item_count:]
+        else:
+            # reading setup stacks from top to bottom
+            parse_line(x, crates)
+
+    answer = []
+    for stack in crates:
+        if len(stack) > 0:
+            answer.append(stack.pop())
+    print('part 2 answer', ''.join(answer))
+
+# solution_part1()
+solution_part2()
