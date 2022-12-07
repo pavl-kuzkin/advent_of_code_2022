4baf92ee64522b2b8abc7dd5e24fe1955f19373c
pavelkuzkin <pavelkuzkin@gmail.com>
2022-12-06 21:11:14 -0800

    P6 part 2

--- 2022/6-find-substring/solved.py
+++ 2022/6-find-substring/solved.py
@@ -1,20 +1,21 @@
-def solution_part1():
+def solution(remember_length):
     f = open("./input.txt", "r")
     for line in f:
         last_four = []
         print(line)
         for idx, char in enumerate(line):
-            if len(last_four) == 4:
+            if len(last_four) == remember_length:
                 del last_four[0]
             last_four.append(char)
             size = len(set(last_four))
             # print(idx+1, size, last_four)
-            if size == 4:
+            if size == remember_length:
                 print('part 1 answer', idx+1)
                 return
 
 
 
-
-solution_part1()
-# solution_part2()
+# part 1
+solution(4)
+# part 2
+solution(14)
