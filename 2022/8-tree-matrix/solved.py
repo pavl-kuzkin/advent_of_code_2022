4667c7a9446cb341b5c412a04c9d6a9683efb7ea
pavelkuzkin <pavelkuzkin@gmail.com>
2022-12-09 13:56:41 -0800

    P8.2 with nodes WIP

--- 2022/8-tree-matrix/solved.py
+++ 2022/8-tree-matrix/solved.py
@@ -1,46 +1,93 @@
+from enum import Enum
+from typing import Optional
+
+
+class Direction(Enum):
+    LEFT = "left"
+    RIGHT = "right"
+    UP = "up"
+    DOWN = "down"
+
+
+class Node:
+    def __init__(self, height: int):
+        self.neighbors = None
+        self.visibility = {
+            Direction.LEFT: None,
+            Direction.RIGHT: None,
+            Direction.DOWN: None,
+            Direction.UP: None,
+        }
+        self.height: int = height
+
+    def __str__(self):
+        return f"{self.height}"
+
+    def connect_to_neighbors(self, matrix: list[list()], x: int, y: int):
+        self.neighbors = {
+            Direction.LEFT: matrix[y][x - 1] if x > 0 else None,
+            Direction.RIGHT: matrix[y][x + 1] if x < (len(matrix[0]) - 1) else None,
+            Direction.DOWN: matrix[y - 1][x] if y > 0 else None,
+            Direction.UP: matrix[y + 1][x] if y < len(matrix) - 1 else None,
+        }
+
+    def is_edge(self):
+        return not any(self.neighbors.values())
+
+    # Lazy load visibility and remember result
+    def is_1d_visible(self, direction: Direction):
+        if self.visibility[direction] is None:
+            self.visibility[direction] = self.__is_1d_visible__(direction)
+        return self.visibility[direction]
+
+    def __is_1d_visible__(self, direction: Direction):
+        neighbor = self.neighbors[direction]
+        if neighbor is None:
+            # if there is no neighbor this node is visible
+            return True
+        elif neighbor.height >= self.height:
+            # if there is a taller neighbor this node is hidden
+            return False
+        else:
+            # there is a neighbor and its shorter
+            return neighbor.is_1d_visible(direction)
+
+    def is_visible(self):
+        return any([self.is_1d_visible(Direction.LEFT),
+                    self.is_1d_visible(Direction.RIGHT),
+                    self.is_1d_visible(Direction.DOWN),
+                    self.is_1d_visible(Direction.UP)
+                    ])
+
+
 class Matrix:
     def __init__(self):
-        self.data = read_input()
-        self.size_x = len(self.data[0])
-        self.size_y = len(self.data)
+        data = read_input()
+        self.nodes: list[list(Node)] = []
+        for col in data:
+            node_column = []
+            self.nodes.append(node_column)
+            for item in col:
+                node_column.append(Node(item))
 
-    def get(self, x: int, y: int):
-        return self.data[y][x]
+        for y in range(len(self.nodes)):
+            for x in range(len(self.nodes)):
+                node: Node = self.nodes[y][x]
+                node.connect_to_neighbors(self.nodes, x, y)
 
-    def is_visible(self, x: int, y: int):
-        val = self.get(x, y)
-        visible = {
-            "left": True,
-            "right": True,
-            "up": True,
-            "down": True
-        }
-        # check left
-        for xi in range(x):
-            if self.get(xi, y) >= val:
-                visible["left"] = False
-        # check right
-        for xi in range(x + 1, self.size_x):
-            if self.get(xi, y) >= val:
-                visible["right"] = False
-        # check up
-        for yi in range(y):
-            if self.get(x, yi) >= val:
-                visible["up"] = False
-        # check down
-        for yi in range(y + 1, self.size_y):
-            if self.get(x, yi) >= val:
-                visible["down"] = False
-        return any(visible.values())
+    def __str__(self):
+        stringified_rows = []
+        for row in self.nodes:
+            stringified_rows.append("".join(map(lambda x: str(x), row)))
+        return "\n".join(stringified_rows)
 
-    def how_many_visible(self):
-        total = 2 * self.size_x + 2 * self.size_y - 4
-        print("is visible", self.is_visible(2, 2))
+    def count_visible(self):
+        total = 0
+        for y in range(len(self.nodes)):
+            for x in range(len(self.nodes[0])):
+                node: Node = self.nodes[y][x]
+                if node.is_visible():
 
-        for xi in range(1, self.size_x-1):
-            for yi in range(1, self.size_y-1):
-                if self.is_visible(xi, yi):
-                    total += 1
         return total
 
 
@@ -56,7 +103,8 @@ def read_input():
 
 def solution_part1():
     matrix = Matrix()
-    print("P1 answer", matrix.how_many_visible())
+    # print(matrix)
 
 
+# 1672
 solution_part1()
