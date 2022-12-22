979ae59e07e46f1f6ed0cf8310b7ff60ce67030b
Pavel Kuzkin <18251378+pakuzkin@users.noreply.github.com>
2022-12-22 10:49:50 -0800

    P15.1 solved

--- 2022/15-beacon-manhattan-radius/main.py
+++ 2022/15-beacon-manhattan-radius/main.py
@@ -1,7 +1,8 @@
 import re
 from collections import defaultdict
 
-INPUT_FILE = "ez-input.txt"
+INPUT_FILE = "input.txt"
+Y_LINE = 10 if INPUT_FILE.startswith("ez-") else 2000000
 
 regex = r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)"
 
@@ -33,12 +34,17 @@ def distance(a, b):
 def circle_area(center, r):
     center_x, center_y = center
     points = []
-    for y in range(center_y - r, center_y + r + 1):
-        # |dx| + |dy| = r, thus |dx| = r - |dy|
-        dy = abs(y - center_y)
-        dx = abs(r-dy)
-        for x in range(center_x - dx, center_x + dx + 1):
-            points.append((x, y))
+    # for y in range(center_y - r, center_y + r + 1):
+    #     # |dx| + |dy| = r, thus |dx| = r - |dy|
+    #     dy = abs(y - center_y)
+    #     dx = abs(r-dy)
+    #     for x in range(center_x - dx, center_x + dx + 1):
+    #         points.append((x, y))
+    dy = abs(center_y - Y_LINE)
+    for x in range(center_x-r, center_x+r+1):
+        dx = abs(center_x - x)
+        if dx + dy <= r:
+            points.append((x, Y_LINE))
     return points
 
 
@@ -51,52 +57,54 @@ def pprint(signal_map):
         ymin = min(ymin, y)
         ymax = max(ymax, y)
     print("dx", xmin, xmax, "dy", ymin, ymax)
-
+    print([x for x in range(xmin, xmax)])
     for y in range(ymin, ymax):
         row = [str(y+500), ": "]
         for x in range(xmin, xmax):
             row.append(signal_map[(x, y)])
-        print(" ".join(row))
+        print("  ".join(row))
 
-
+# i  i  i  i  B  g  g  g  g  g  g  g  g  g  d  g  g  l  j  j  j  j  j  j  j  j  j
+# i  i  i  i  B  g  g  g  g  g  g  g  g  g  d  g  g  l  j  j  j  j  j  j  j  j  j  .
+# #  #  #  #  B  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
+# #  #  #  #  B  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #  #
 
 def problemOne():
 
     # Note y-axis is inverted rocks start at 500,0 and fall "down" to 500,1 etc
     lines = open(INPUT_FILE, "r").readlines()
     signal_map = defaultdict(lambda: '.')
+    signal_map2 = defaultdict(lambda: '.')
     xmin, xmax = 0, 0
+    c = 'a'
     for line in lines:
         points = decypher_line(line.strip())
-        print(points, " <- ", line.strip())
+        # print(c, points, " <- ", line.strip())
         xs, ys, xb, yb = points[0], points[1], points[2], points[3]
         beacon = (xb, yb)
         sensor = (xs, ys)
         signal_map[beacon] = 'B'
         signal_map[sensor] = 'S'
-        print("getting dist")
+        # signal_map2[beacon] = 'B'
+        # signal_map2[sensor] = 'S'
         d = distance(sensor, beacon)
-        print("getting coverage")
         coverage = circle_area(sensor, d)
         print("getting #s")
         for point in coverage:
             if signal_map[point] == '.':
                 signal_map[point] = '#'
+                # signal_map2[point] = c
                 xmin = min(xmin, point[0])
                 xmax = max(xmax, point[0])
+        c = chr(ord(c)+1)
 
     print("alg range", xmin, xmax)
-    pprint(signal_map)
-
-    # for (x, y) in signal_map.keys():
-    #     xmin = min(xmin, x)
-    #     xmax = max(xmax, x)
+    # pprint(signal_map)
+    # pprint(signal_map2)
 
     count_not_beacon = 0
-    ez_y = 10
-    p1_y = 2000000
-    for x in range(xmin, xmax):
-        if signal_map[(x, ez_y)] == '#':
+    for x in range(xmin, xmax+10):
+        if signal_map[(x, Y_LINE)] == '#':
             count_not_beacon += 1
 
     print("P1 ans", count_not_beacon)
