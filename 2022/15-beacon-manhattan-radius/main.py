06b1ab5051b132a4b1f8423025a2ad04427e4a4a
Pavel Kuzkin <18251378+pakuzkin@users.noreply.github.com>
2022-12-22 20:44:55 -0800

    P15.2 solved

--- 2022/15-beacon-manhattan-radius/main.py
+++ 2022/15-beacon-manhattan-radius/main.py
@@ -1,8 +1,11 @@
 import re
 from collections import defaultdict
+from functools import cmp_to_key
 
 INPUT_FILE = "input.txt"
 Y_LINE = 10 if INPUT_FILE.startswith("ez-") else 2000000
+MIN = 0
+MAX = 20 if INPUT_FILE.startswith("ez") else 4000000
 
 regex = r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)"
 
@@ -41,7 +44,7 @@ def circle_area(center, r):
     #     for x in range(center_x - dx, center_x + dx + 1):
     #         points.append((x, y))
     dy = abs(center_y - Y_LINE)
-    for x in range(center_x-r, center_x+r+1):
+    for x in range(center_x - r, center_x + r + 1):
         dx = abs(center_x - x)
         if dx + dy <= r:
             points.append((x, Y_LINE))
@@ -59,7 +62,7 @@ def pprint(signal_map):
     print("dx", xmin, xmax, "dy", ymin, ymax)
     print([x for x in range(xmin, xmax)])
     for y in range(ymin, ymax):
-        row = [str(y+500), ": "]
+        row = [str(y + 500), ": "]
         for x in range(xmin, xmax):
             row.append(signal_map[(x, y)])
         print("  ".join(row))
@@ -74,18 +77,19 @@ def read_input():
         xs, ys, xb, yb = points[0], points[1], points[2], points[3]
         beacon = (xb, yb)
         sensor = (xs, ys)
-        exclusion_zones.append((sensor, beacon))
+        d = distance(sensor, beacon)
+        exclusion_zones.append((sensor, beacon, d))
     return exclusion_zones
+
 
 def problemOne():
     signal_map = defaultdict(lambda: '.')
     xmin, xmax = 0, 0
 
     exclusion_zones = read_input()
-    for (sensor, beacon) in exclusion_zones:
+    for (sensor, beacon, radius) in exclusion_zones:
         signal_map[sensor] = 'S'
         signal_map[beacon] = 'B'
-        radius = distance(sensor, beacon)
         print("calculating zone", sensor, radius)
         coverage = circle_area(sensor, radius)
         # print("getting #s")
@@ -101,18 +105,121 @@ def problemOne():
     # pprint(signal_map2)
 
     count_not_beacon = 0
-    for x in range(xmin, xmax+10):
+    for x in range(xmin, xmax + 10):
         if signal_map[(x, Y_LINE)] == '#':
             count_not_beacon += 1
 
     print("P1 ans should be 5688618", count_not_beacon)
 
 
-problemOne()
+# problemOne()
+
+def in_any_zone(x, y, exclusion_zones):
+    for (sensor, beacon, radius) in exclusion_zones:
+        d = distance((x, y), sensor)
+        if d <= radius:
+            return True
+    return False
+
+
+def can_combine(r1, r2):
+    x1, y1 = r1
+    x2, y2 = r2
+    return x2 <= y1 <= y2 or x1 <= y2 <= y1 or (y1 < y2 and y1 + 1 == x2) or (y2 < y1 and y2 + 1 == x1)
+
+
+def combine_range(r1, r2):
+    x1, y1 = r1
+    x2, y2 = r2
+    return min(x1, x2), max(y1, y2)
+
+
+def combine_range_recr(range_list):
+    for i in range(len(range_list) - 1):
+        r1 = range_list[i]
+        r2 = range_list[i + 1]
+        if can_combine(r1, r2):
+            range_list[i + 1] = combine_range(r1, r2)
+            range_list[i] = None
+
+    return list(filter(lambda x: x is not None, range_list))
+
+
+def sort_ranges(range_list):
+    return list(reversed(sorted(range_list, key=cmp_to_key(lambda r1, r2: r2[0] - r1[0]))))
+
+
+def find_x(exclusion_zones):
+    for x in range(MIN, MAX):
+        # figure out y-ranges for each zone
+        print("x", x)
+        ranges = [(0, 1)]
+        for (sensor, beacon, radius) in exclusion_zones:
+            cx, cy = sensor
+            dx = abs(cx - x)
+            if dx > radius:
+                continue
+            dy = radius - dx
+            range_min, range_max = cy - dy, cy + dy
+            new_range = (range_min, range_max)
+            combined = False
+            for (i, prev_r) in enumerate(ranges):
+                if can_combine(prev_r, new_range):
+                    combined = True
+                    ranges[i] = combine_range(prev_r, new_range)
+                    break
+            if not combined:
+                ranges.append(new_range)
+                ranges = sort_ranges(ranges)
+
+            # print("-zone", sensor, radius, "range", range_min, range_max)
+            # print("ranges", ranges)
+        # combine ranges into one
+        ranges = combine_range_recr(ranges)
+        if len(ranges) > 1:
+            print("FOUND X", x)
+            return x
+        print("final ranges", ranges)
+
+
+def find_y(exclusion_zones):
+    for y in range(MIN, MAX):
+        # figure out y-ranges for each zone
+        print("y", y)
+        ranges = [(0, 1)]
+        for (sensor, beacon, radius) in exclusion_zones:
+            cx, cy = sensor
+            dy = abs(cy - y)
+            if dy > radius:
+                continue
+            dx = radius - dy
+            range_min, range_max = cx - dx, cx + dx
+            new_range = (range_min, range_max)
+            combined = False
+            for (i, prev_r) in enumerate(ranges):
+                if can_combine(prev_r, new_range):
+                    combined = True
+                    ranges[i] = combine_range(prev_r, new_range)
+                    break
+            if not combined:
+                ranges.append(new_range)
+                ranges = sort_ranges(ranges)
+
+            # print("-zone", sensor, radius, "range", range_min, range_max)
+            # print("ranges", ranges)
+        # combine ranges into one
+        ranges = combine_range_recr(ranges)
+        if len(ranges) > 1:
+            print("FOUND Y", y)
+            return y
+        print("final ranges", ranges)
 
 
 def problemTwo():
-    print("P2 ans", 0)
+    exclusion_zones = read_input()
+    x = find_x(exclusion_zones)
+    y = find_y(exclusion_zones)
 
+    print("P2 (ans = 12625383204261)", x * 4000000 + y)
 
 problemTwo()
